29f2c863c6442d612f2abd5142b8c6ff4ab85a26
munbohr <85949748+munbohr@users.noreply.github.com>
2022-04-01 19:44:40 +0900

    test middleware & update impormation

--- src/server.js
+++ src/server.js
@@ -4,11 +4,15 @@ const PORT = 3000;
 
 const app = express();
 
-const handleHome = (req, res)=> {
-    console.log(res);
+const handleHome = (req, res, next)=> {
+    console.log("i'm in the Middleware");
+    next();
 }
-
+const handleLogin = (req, res) => {
+    return res.send({ messege : "Login page"});
+}
 app.get("/", handleHome );
+app.get("/login", handleLogin );
 
 const handleListening = () => {console.log(`✅ server Listening on port  http://localhost:${PORT} 🚀 `);}
 
