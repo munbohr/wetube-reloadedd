e2a659543b151b68e78dc45ad4bf55c3ffdf0675
munbohr <85949748+munbohr@users.noreply.github.com>
2022-04-01 19:46:49 +0900

    use middleware

--- src/server.js
+++ src/server.js
@@ -4,14 +4,18 @@ const PORT = 3000;
 
 const app = express();
 
-const handleHome = (req, res, next)=> {
+const gossipMiddleware = (req, res, next) => {
     console.log("i'm in the Middleware");
     next();
+}
+
+const handleHome = (req, res, next)=> {
+    return res.send("Home")
 }
 const handleLogin = (req, res) => {
     return res.send({ messege : "Login page"});
 }
-app.get("/", handleHome );
+app.get("/",gossipMiddleware, handleHome );
 app.get("/login", handleLogin );
 
 const handleListening = () => {console.log(`✅ server Listening on port  http://localhost:${PORT} 🚀 `);}
