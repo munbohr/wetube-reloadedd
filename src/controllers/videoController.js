9e68e42b71e3d9f5e15cb6281a823b70c10b22aa
munbohr <85949748+munbohr@users.noreply.github.com>
2022-04-03 01:25:43 +0900

    before i use Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,10 @@
-export const trending = (req, res) => {res.send("Home Page Videos")}
+export const trending = (req, res) => {res.send("<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>"
+)}
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`)}
+    return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch Video #${req.params.id}</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>`)}
 export const edit = (req, res) =>{
     console.log(req.params);
-    return res.send("Edit")}
+    return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit Video #${req.params.id}</h1><footer>&copy;2022 Wetube -  All rights reserved</footer></body></html>`)}
 export const search = (req, res) => {res.send("Search")}
 export const upload = (req, res) => {res.send("Upload")}
 export const deleteVideo = (req, res) => {
